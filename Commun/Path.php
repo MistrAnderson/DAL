81c39733cac8c2352f1e9ede5ba6238166c10e3e
mstrndrsn <noteo.gaming@protonmail.com.com>
2024-05-28 01:45:20 +0200

    [UPDATE] Formating of files

--- Commun/Path.php
+++ Commun/Path.php
@@ -1,14 +1,18 @@
 <?php
-    class Path {
-        static function PATH($file="") {
-            return $_SERVER["DOCUMENT_ROOT"]."/".$file;
-        }
+class Path
+{
+  static function PATH($file = "")
+  {
+    return $_SERVER["DOCUMENT_ROOT"] . "/" . $file;
+  }
 
-        static function SECURE_PATH($file="") {
-            return self::PATH()."../".$file;
-        }
+  static function SECURE_PATH($file = "")
+  {
+    return self::PATH() . "../" . $file;
+  }
 
-        static function CREDENTIALS($file="") {
-            return self::SECURE_PATH()."credentials/".$file;
-        }
-    }
+  static function CREDENTIALS($file = "")
+  {
+    return self::SECURE_PATH() . "credentials/" . $file;
+  }
+}
